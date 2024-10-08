87413e6099cfc65b95a73af9eb4f063c21d7c0d6
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2024-10-08 23:37:14 +0200

    v0.0.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,23 +1,3 @@
 # Changelog
 
 ## [Unreleased]
-
-## [0.0.2] - 2024-10-08
-### Patch
-- Updated patch version from 0.0.1 to 0.0.2
-
-## [0.0.1] - 2024-10-08
-### Rollback
-- Rolled back from v0.0.2 to v0.0.1
-
-## [0.0.2] - 2024-10-07
-### Rollback
-- Rolled back from v0.0.3 to v0.0.2
-
-## [0.0.3] - 2024-10-07
-### Patch
-- Updated patch version from 0.0.2 to 0.0.3
-
-## [0.0.2] - 2024-10-07
-### Patch
-- Updated patch version from 0.0.1 to 0.0.2
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,12 @@
-import path from "https://deno.land/std@0.116.0/node/path.ts";
-import express from "npm:express";
-import cors from "npm:cors";
-
-
 import { Logger } from '../.core/logger.ts';
 import { CONFIG } from './config.ts';
 import { STATE } from './state.ts';
 
 export async function main(): Promise<void> {
   Logger.log('hello world from ./src/main.ts!');
-  
+  Logger.log(`config: ${JSON.stringify(CONFIG)}`);
+  STATE.PORT = 60002;
+  Logger.log(`state: ${JSON.stringify(STATE)}`);
+
   await Promise.resolve();
 }
